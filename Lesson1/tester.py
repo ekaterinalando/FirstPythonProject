a5c8e66c6af6e73c837c19b5643d26c5b88faef8
Ekaterina Kotets <katya@Ekaterinas-MacBook.local>
2021-11-18 20:28:58 +0200

    Initial commit

---
+++ Lesson1/tester.py
@@ -0,0 +1,11 @@
+int_var = 500
+float_var = 50.0
+string_var_text = 'Hello WORLD'
+string_var_num = '123.2'
+bool_var = True
+none_type = None
+
+print(string_var_text + str(int_var))
+print(type(int_var))
+
+print(float_var + float(string_var_num))
